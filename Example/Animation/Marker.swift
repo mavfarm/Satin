64d87c4762a5dfc4ca570ee388d6ef304b4752a2
Colin Duffy <colin@tomorrowevening.com>
2019-12-30 16:17:56 -0800

    public/accessible vars

---
+++ Example/Animation/Marker.swift
@@ -0,0 +1,9 @@
+//
+//  Marker.swift
+//  Example iOS
+//
+//  Created by Colin Duffy on 12/29/19.
+//  Copyright © 2019 Reza Ali. All rights reserved.
+//
+
+import Foundation
---
+++ Example/Animation/Timer.swift
@@ -0,0 +1,9 @@
+//
+//  Timer.swift
+//  Example iOS
+//
+//  Created by Colin Duffy on 12/29/19.
+//  Copyright © 2019 Reza Ali. All rights reserved.
+//
+
+import Foundation
