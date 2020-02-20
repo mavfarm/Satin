97756b1929aab423a5fb3e302474afb7e4312efe
Colin Duffy <colin@tomorrowevening.com>
2020-02-20 13:47:27 -0800

    mostly lerp added plus a few more helpers

--- Satin/Extensions/Simd+Extensions.swift
+++ Satin/Extensions/Simd+Extensions.swift
@@ -24,8 +24,53 @@ extension simd_quatf: Codable {
         try container.encode(self.vector.z, forKey: .z)
         try container.encode(self.vector.w, forKey: .w)
     }
+    
+    public func lerp(_ end: simd_quatf, _ progress: Float) -> simd_quatf {
+        return simd_slerp(self, end, progress)
+    }
 
     private enum CodingKeys: String, CodingKey {
         case x,y,z,w
+    }
+}
+
+extension simd_float2 {
+    public func lerp(_ end: simd_float2, _ progress: Float) -> simd_float2 {
+        return self + (end - self) * progress
+    }
+}
+
+extension simd_float3 {
+    public func lerp(_ end: simd_float3, _ progress: Float) -> simd_float3 {
+        return self + (end - self) * progress
+    }
+    
+    static func toHex(_ hex: Int) -> simd_float3 {
+        let dividend = Float(255)
+        return simd_make_float3(
+            Float( hex >> 16 & 255 ) / dividend,
+            Float( hex >> 8 & 255 ) / dividend,
+            Float( hex & 255 ) / dividend
+        )
+    }
+}
+
+extension simd_float4 {
+    public func lerp(_ end: simd_float4, _ progress: Float) -> simd_float4 {
+        return self + (end - self) * progress
+    }
+    
+    public func xyz() -> simd_float3 {
+        return simd_make_float3(self.x, self.y, self.z)
+    }
+}
+
+extension matrix_float4x4 {
+    func upper_left_3x3() -> matrix_float3x3 {
+        return matrix_float3x3(
+            simd_make_float3(self.columns.0.x, self.columns.0.y, self.columns.0.z),
+            simd_make_float3(self.columns.1.x, self.columns.1.y, self.columns.1.z),
+            simd_make_float3(self.columns.2.x, self.columns.2.y, self.columns.2.z)
+        )
     }
 }
