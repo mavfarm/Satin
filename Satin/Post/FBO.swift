06826cb531a569a748dced95d1220f4d1619d4e0
Colin Duffy <colin@tomorrowevening.com>
2020-02-20 19:06:37 -0800

    Post Processing added

---
+++ Satin/Post/FBO.swift
@@ -0,0 +1,34 @@
+//
+//  FBO.swift
+//  Satin
+//
+//  Created by Colin Duffy on 2/20/20.
+//  Copyright © 2020 Colin Duffy. All rights reserved.
+//
+
+import Metal
+
+class FBO {
+    
+    var writeBuffer: MTLTexture
+    var readBuffer: MTLTexture
+    var format: MTLPixelFormat
+    
+    init(width: Int, height: Int, format: MTLPixelFormat) {
+        self.writeBuffer = Pass.createRenderTarget(width: width, height: height, format: format)
+        self.readBuffer = Pass.createRenderTarget(width: width, height: height, format: format)
+        self.format = format
+    }
+    
+    func swapBuffers() {
+        let temp = self.readBuffer
+        self.readBuffer = self.writeBuffer
+        self.writeBuffer = temp
+    }
+    
+    func resize(_ width: Int, _ height: Int) {
+        self.writeBuffer = Pass.createRenderTarget(width: width, height: height, format: format)
+        self.readBuffer = Pass.createRenderTarget(width: width, height: height, format: format)
+    }
+    
+}
---
+++ Satin/Post/PostProcessor.swift
@@ -0,0 +1,83 @@
+//
+//  PostProcessor.swift
+//  Satin
+//
+//  Created by Colin Duffy on 2/20/20.
+//  Copyright © 2020 Colin Duffy. All rights reserved.
+//
+
+import Metal
+import MetalKit
+
+class PostProcessor {
+    
+    var passes: [Pass]
+    var fbo: FBO
+    var enabled: Bool = true
+    
+    /// Internal
+    internal var width: Float
+    internal var height: Float
+    
+    init() {
+        self.passes = []
+        self.width = Float(UIScreen.main.bounds.width)
+        self.height = Float(UIScreen.main.bounds.height)
+        self.fbo = FBO(width: Int(width), height: Int(height), format: .bgra8Unorm_srgb)
+    }
+    
+    func add(_ pass: Pass) {
+        passes.append(pass)
+        pass.resize((width, height))
+    }
+    
+    func swapBuffers() {
+        self.fbo.swapBuffers()
+    }
+    
+    func update() {
+        for pass in passes {
+            if pass.enabled {
+                pass.update()
+            }
+        }
+    }
+    
+    func draw(_ view: MTKView, _ renderPassDescriptor: MTLRenderPassDescriptor, _ commandBuffer: MTLCommandBuffer) {
+        let totalPasses = passes.count
+        if !enabled && totalPasses > 0 {
+            passes[0].draw(view, renderPassDescriptor, commandBuffer, self.fbo.readBuffer)
+            return
+        }
+        
+        var index: Int = 0
+        let lastPass = totalPasses - 1
+        for pass in passes {
+            if pass.enabled {
+                
+                if pass.renderToScreen || index == lastPass {
+                    pass.draw(view, renderPassDescriptor, commandBuffer, self.fbo.readBuffer)
+                } else {
+                    pass.draw(view, renderPassDescriptor, commandBuffer, self.fbo.readBuffer, self.fbo.writeBuffer)
+                }
+                
+                if pass.needsSwap {
+                    swapBuffers()
+                }
+            }
+            index += 1
+        }
+    }
+    
+    open func resize(_ size: (width: Float, height: Float)) {
+        self.width = size.width
+        self.height = size.height
+        
+        self.fbo.resize(Int(width), Int(height))
+        
+        for pass in passes {
+            pass.resize(size)
+        }
+    }
+    
+}
